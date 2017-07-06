b58cb4bd1dc1e566d1e8b3e905bdf41866ab7785
aisha <a.blfgeh1@ncl.ac.uk>
2017-07-06 13:01:23 +0100

    Add filter function to read interned entities

--- src/mfoem/em-ont.clj
+++ src/mfoem/em-ont.clj
@@ -1,15 +1,14 @@
 (ns mfoem.em-ont
   (:use [tawny.owl]
-        [tawny.pattern]
-        [tawny.repl]
         [tawny.reasoner])
   (:require [clojure.string :as str]
             [tawny owl read]))
 
 (tawny.read/defread MFOEM
   :location (tawny.owl/iri (clojure.java.io/resource "MFOEM.owl"))
-  :prefix "em-ont"
+  :prefix "em-ont:"
   :iri "http://purl.obolibrary.org/obo/MFOEM.owl"
+  :filter (partial
+           tawny.read/iri-starts-with-filter
+           "http://purl.obolibrary.org/obo/MFOEM")
   :transform #'tawny.read/label-transform)
-
-(save-ontology "em.owl" :owl)
